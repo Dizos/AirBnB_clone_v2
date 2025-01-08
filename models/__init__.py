35ca31d50ddde5c78f8f8c31c1176219af4f9d45
Tando Mjekula <tandomjekula@gmail.com>
2025-01-08 16:14:00 +0200

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-"""Initialize models package"""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+"""__init__ method for models package, or
+Module for FileStorage autoinit."""
 
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'State': State,
-    'City': City,
-    'Amenity': Amenity,
-    'Place': Place,
-    'Review': Review
-}
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
